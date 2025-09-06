a65591f4ed2d3d34d0a52dd8a802609c1ad46ca4
AmeerAg <73844220+AmeerAg7@users.noreply.github.com>
2025-09-06 14:17:00 +0300

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -43,7 +43,7 @@ public class PersonService {
 		p = new Person();
 		p.setId("5");
 		p.setAge(44);
-		p.setFirstName("webhookFirewallTes");
+		p.setFirstName("webhook");
 		p.setLastName("test");
 		persons.add(p);
 
