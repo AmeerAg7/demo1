932897ef59b6821e2e6e550d083195d2efa4d019
AmeerAg <73844220+AmeerAg7@users.noreply.github.com>
2025-10-07 17:47:27 +0300

    Update PersonService.java 5th person with seemio 1

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -41,9 +41,9 @@ public class PersonService {
 		persons.add(p);
 		
 		p = new Person();
-		p.setId("6");
+		p.setId("5");
 		p.setAge(44);
-		p.setFirstName("webhookTest1");
+		p.setFirstName("webhookTestSeemIo1");
 		p.setLastName("test");
 		persons.add(p);
 
