87d67e5abeb9f8537e20b603de2e45c786ba9e92
AmeerAg <73844220+AmeerAg7@users.noreply.github.com>
2025-10-13 09:10:47 +0300

    Test adding email notification sendgrid by Ameer 2 - to ds mail

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -3,7 +3,7 @@ package com.example.model;
 public class Person {
 	//Test comment added
 	// add comment 2
-	// test adding email notification by Ameer
+	// test adding email notification by Ameer 2
 	String id;
 	String firstName;
 	String lastName;
